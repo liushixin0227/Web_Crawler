2c76c9fc294a3fa437fe3cbd9bf3356ce6e55be0
liushixin <522273565@qq.com>
2018-09-19 17:23:31 +0800

    前期

--- test.py
+++ test.py
@@ -1,19 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-# @Time    : 2018/9/18 0018 17:06
-# @Author  : Liushixin
-# @Site    : 
-# @File    : test.py
-# @Software: PyCharm
-import lxml
-from selenium import webdriver
-import aiohttp, aiodns
-from bs4 import BeautifulSoup
-import pyquery
-
-# browser = webdriver.PhantomJS(executable_path=r'C:\SoftWare\Tools\phantomjs-2.1.1-windows\bin\phantomjs.exe')
-# browser.get('https://www.baidu.com')
-# print(browser.current_url)
-
-soup = BeautifulSoup('<p> Hello </p>', 'lxml')
-print(soup.p.string)
+from scrapyd_api import ScrapydAPI
+scrapyd = ScrapydAPI('http://localhost:6800')
+print(scrapyd.list_projects())
